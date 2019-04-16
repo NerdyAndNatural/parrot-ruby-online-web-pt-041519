05296321e2afe547fe10cdcdc4fe7f1b148371c6
Sarah Williams <srwilliams8678@gmail.com>
2019-04-16 11:22:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
+def parrot (SquawK)
 puts "Squawk!"
 return "Squawk!"
-puts "Pretty bird!"
+end
+
+def parrot (phrase)
+  puts "Pretty bird!"
 end
